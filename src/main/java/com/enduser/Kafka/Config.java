c3a4e8f6ea146214c3e8dc7bce6fe5dc747b3e88
Ritik Raj <ritikraj6120@gmail.com>
2023-12-01 23:56:50 +0530

    initial commit

---
+++ src/main/java/com/enduser/Kafka/Config.java
@@ -0,0 +1,12 @@
+package com.enduser.Kafka;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Configuration
+public class Config {
+    @KafkaListener(topics=AppConstants.LOCATION_UPDATE_TOPIC,groupId = AppConstants.GROUP_ID)
+    public void updatedLocation(String value){
+
+    }
+}
