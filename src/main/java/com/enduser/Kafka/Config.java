18476837fe615c00271dbf9024c07dc781a44b19
Ritik Raj <ritikraj6120@gmail.com>
2023-12-02 00:13:52 +0530

    corrected serializer

--- src/main/java/com/enduser/Kafka/Config.java
+++ src/main/java/com/enduser/Kafka/Config.java
@@ -7,6 +7,6 @@ import org.springframework.kafka.annotation.KafkaListener;
 public class Config {
     @KafkaListener(topics=AppConstants.LOCATION_UPDATE_TOPIC,groupId = AppConstants.GROUP_ID)
     public void updatedLocation(String value){
-
+        System.out.println(value);
     }
 }
